b0785764a93dc6480550ed0e3d7a764be5c1b226
Russell West <Russ West>
2019-07-31 20:06:48 -0700

    D11

--- 2018/d11.fs
+++ 2018/d11.fs
@@ -2,7 +2,7 @@
 
 type XYP = { X : int ; Y : int ; P : int }
 
-let p size sn =
+let p1 size sn =
     (*
         Find the fuel cell's rack ID, which is its X coordinate plus 10.
         Begin with a power level of the rack ID times the Y coordinate.
@@ -37,3 +37,73 @@ let p size sn =
     |> Seq.windowed 3
     |> Seq.map sumWindow
     |> Seq.maxBy (fun { P = p } -> p)
+
+let p2 size sn =
+    // compute square (memoized)
+    let power = new System.Collections.Concurrent.ConcurrentDictionary<int*int*int, int>()
+    let rec computeSquare x y s =
+        let compute x y s =
+            if s = 1 then
+                let rackId = x + 10
+                let powerLevel = ((rackId * y) + sn) * rackId
+                let hundreds = powerLevel / 100 % 10
+
+                hundreds - 5
+            elif s % 2 = 0 then
+                let a = s / 2
+
+                seq {
+                    //   (a) > a
+                    yield computeSquare ( x     ) ( y     ) a
+                    
+                    //   (a) v a
+                    yield computeSquare ( x + a ) ( y     ) a
+
+                    //   (a) < a
+                    yield computeSquare ( x + a ) ( y + a ) a
+                    
+                    //   (a)
+                    yield computeSquare ( x     ) ( y + a ) a
+
+                } |> Seq.sum
+            else
+                let a = (s + 1) / 2
+                let b = a - 1
+                
+                seq { 
+                    //   (a) \  a ... 1
+                    yield computeSquare x y a
+
+                    //   (b) ^< b ... 2
+                    yield computeSquare ( x + a ) ( y + a ) b
+
+                    yield computeSquare ( x + a ) ( y + 1 ) b
+                    yield computeSquare ( x + 1 ) ( y + a ) b
+                    
+                    //   (c) >v c ... b
+                    for n = 0 to b-1 do
+                        yield computeSquare ( x + a + n ) ( y         ) 1
+                        yield computeSquare ( x         ) ( y + a + n ) 1
+                }
+                |> Seq.sum
+
+        let k = x,y,s
+        match power.TryGetValue(k) with
+        | true, v ->
+            //printfn "[M] %A %A %A = %A" x y s v
+            v
+        | false, _ ->
+            let v = compute x y s
+            power.TryAdd(k, v) |> ignore
+            // printfn "[C] %A %A %A = %A" x y s v
+            v
+
+    seq {
+        for s = 1 to size do
+            let bound = size - s + 1
+            for x = 1 to bound do
+                System.Console.Title <- sprintf "%3i %3i" s x
+                for y = 1 to bound do
+                    yield (x,y,s)
+    }
+    |> Seq.maxBy (function (x,y,s) -> computeSquare x y s)
